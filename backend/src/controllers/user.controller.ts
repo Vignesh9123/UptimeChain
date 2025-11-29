cb9a8d5b8ee782f25a31a0e6dad912e78cfcf1c7
Vignesh D <vignesh.d9123@gmail.com>
2025-11-29 14:52:16 +0530

    Add auth routes

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,5 +1,33 @@
 import type { Request, Response } from "express"
 import {prisma} from '../lib/prisma'
+import * as z from 'zod'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { env } from "../config"
+
+const loginSchema = z.object({
+    email: z
+        .email(),
+    password: z
+        .string()
+        .min(8, "Password should be at least 8 characters long")
+        .max(20, "Password should be at most 50 characters long")
+    
+})
+
+const registerSchema = z.object({
+    email: z
+        .email(),
+    name: z
+        .string()
+        .min(3, "Name should be at least 3 characters long")
+        .max(20, "Name should be at most 50 characters long"),
+    password: z
+        .string()
+        .min(8, "Password should be at least 8 characters long")
+        .max(20, "Password should be at most 50 characters long")
+    
+})
 
 export const getCurrentUser = async (req: Request, res: Response) => {
     try {
@@ -26,8 +54,51 @@ export const getCurrentUser = async (req: Request, res: Response) => {
 
 export const loginUser = async (req: Request, res: Response) => {
     try {
-        
+        const cleanedBody = loginSchema.parse(req.body);
+        const user = await prisma.user.findFirst({
+            where: {
+                email: cleanedBody.email
+            }
+        })
+        if(!user) {
+            return res.status(404).json({message: "User not found"})
+        }
+        if(!user.password) {
+            return res.status(404).json({message: "User not found"})
+        }
+        const isPasswordMatching = await bcrypt.compare(cleanedBody.password, user.password);
+        if(!isPasswordMatching) {
+            return res.status(401).json({message: "Unauthorized"})
+        }
+        const token = jwt.sign({id: user.id}, env.JWT_SECRET_KEY, {expiresIn: "1d"});
+        return res.status(200).json({message: "Login successful", token});   
     } catch (error) {
-        
+        return res.status(500).json({message: (error as any)?.message || "Something went wrong"})
+    }
+}
+
+export const registerUser = async (req: Request, res: Response) => {
+    try {
+        const cleanedBody = registerSchema.parse(req.body);
+        const existingUser = await prisma.user.findFirst({
+            where: {
+                email: cleanedBody.email
+            }
+        })
+        if(existingUser) {
+            return res.status(409).json({message: "User already exists"})
+        }
+        const hashedPassword = await bcrypt.hash(cleanedBody.password, 10);
+        const user = await prisma.user.create({
+            data: {
+                email: cleanedBody.email,
+                name: cleanedBody.name,
+                password: hashedPassword
+            }
+        })
+        return res.status(201).json({message: "User created", data: user})   
+    }
+    catch (error) {
+        return res.status(500).json({message: (error as any)?.message || "Something went wrong"})
     }
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares";
-import { getCurrentUser } from "../controllers";
+import { getCurrentUser, loginUser, registerUser } from "../controllers";
 
 const router = Router()
 
 router.route('/current').get(authMiddleware, getCurrentUser)
+router.route('/login').post(loginUser)
+router.route('/register').post(registerUser)
 
 export {router as userRouter}
