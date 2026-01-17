38ef0e3be42511603795bba4465833ca0d517250
Vignesh D <vignesh.d9123@gmail.com>
2026-01-17 12:17:31 +0000

    Add result-submit route and update fetch task logic

--- queue-api/index.ts
+++ queue-api/index.ts
@@ -15,11 +15,22 @@ const taskSchema = z.object({
     roundTimestamp: z.number().int().min(0).optional() // TODO: Remove optional afterwards
 })
 
+const resultSchema = z.object({
+    data: z.object({
+        targetUrl: z.url(),
+        latency: z.number().int().min(0),
+        certificateExpiryTs: z.number().int().min(0)
+    }),
+    signature: z.string(),
+    publicKey: z.string()
+})
+
 app.post("/api/push-task", async (req, res)=>{
     try {
         const {body} = req
         const cleanedBody = taskSchema.parse(body)
         await client.rPush("task-queue", JSON.stringify(cleanedBody))
+        console.log("Task pushed successfully", cleanedBody)
         return res
         .status(200)
         .json({
@@ -39,9 +50,9 @@ app.get("/api/fetch-task", async (req, res) => {
     try {
       const now = Date.now()
       const TWO_MINUTES = 2 * 60 * 1000
-  
+      let index = 0;
       while (true) {
-        const taskStr = await client.lIndex("task-queue", 0)
+        const taskStr = await client.lIndex("task-queue", index)
   
         if (!taskStr) {
           return res.status(200).json({
@@ -69,13 +80,40 @@ app.get("/api/fetch-task", async (req, res) => {
           await client.lPop("task-queue")
           continue
         }
-          return res.status(200).json({
-          task
+        const checkValidatorHasSubmitted = await client.get(`result:${task.targetUrl + req.headers["x-public-key"]}`)
+        if(checkValidatorHasSubmitted){
+          index++;
+          continue
+        }
+        return res.status(200).json({
+        task
         })
       }
     } catch (e) {
       return res.status(500).json({
         message: "Failed to fetch task"
+      })
+    }
+})
+
+app.post("/api/result-submit", async (req, res) => {
+    try {
+      const {body} = req
+      const cleanedBody = resultSchema.parse(body)
+      console.log("Result submitted successfully", cleanedBody)
+      await client.setEx(`result:${cleanedBody.data.targetUrl + req.headers["x-public-key"]}`, 2 * 60, JSON.stringify(cleanedBody))
+      // push to result queue 
+      return res
+      .status(200)
+      .json({
+        message:"Result submitted successfully"
+      })
+    }
+    catch(e){
+      return res
+      .status(500)
+      .json({
+        message:"Result submission failed"
       })
     }
 })
